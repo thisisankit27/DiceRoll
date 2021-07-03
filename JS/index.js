79e0d89dd5be68fde089d301c56978310fec1c33
thisisankit27 <thisisankitsrivastava@gmail.com>
2021-07-03 13:47:35 +0530

    Dice ROll + Button Functionality
    Signed-off-by: thisisankit27 <thisisankitsrivastava@gmail.com>

---
+++ JS/index.js
@@ -0,0 +1,17 @@
+function diceRoll() {
+    var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+    var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+    document.querySelectorAll(".dice img")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
+    document.querySelectorAll(".dice img")[1].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+    if (randomNumber1 == randomNumber2) {
+        document.querySelector(".container-fluid h1").textContent = "Draw!";
+    }
+    else if (randomNumber1 > randomNumber2) {
+        document.querySelector(".container-fluid h1").textContent = "🚩Player 1 Wins!";
+    }
+    else {
+        document.querySelector(".container-fluid h1").textContent = "Player 2 Wins!🚩";
+    }
+}
