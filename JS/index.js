b94266d509df56bd607e5ae57112b0ee5dd5bb32
thisisankit27 <thisisankitsrivastava@gmail.com>
2021-07-04 08:32:34 +0530

    added gif feature
    Signed-off-by: thisisankit27 <thisisankitsrivastava@gmail.com>

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,7 @@
-function diceRoll() {
+function dicing() {
+    document.querySelector(".dice-row").classList.toggle("hidden");
+    document.querySelector(".gif").classList.toggle("hidden");
+
     var randomNumber1 = Math.floor(Math.random() * 6 + 1);
     var randomNumber2 = Math.floor(Math.random() * 6 + 1);
 
@@ -14,4 +17,15 @@ function diceRoll() {
     else {
         document.querySelector(".container-fluid h1").textContent = "Player 2 Wins!🚩";
     }
+
+    document.querySelector("button.button").textContent = "Roll";
+    document.querySelector("button.button").setAttribute("onclick", "rolling()");
+}
+
+function rolling() {
+    document.querySelector("button.button").innerHTML = "Dice";
+    document.querySelector("h1").innerHTML = "Click to Dice";
+    document.querySelector(".dice-row").classList.toggle("hidden");
+    document.querySelector(".gif").classList.toggle("hidden");
+    document.querySelector("button.button").setAttribute("onclick", "dicing()");
 }
